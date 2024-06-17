82470fb3f082816f56a88bb18e68a0ec99aacd9c
Fabricio Curvello <fabricio.desenvolvedor@gmail.com>
2024-06-16 21:25:57 -0300

    Loop iterators

--- src/05-loop-iterators.kt
+++ src/05-loop-iterators.kt
@@ -6,16 +6,17 @@ class Animal(val name: String)
 
 class Zoo(val animals: List<Animal>) {
 
-    operator fun iterator(): Iterator<Animal> {             // 1
-        return animals.iterator()                           // 2
+    operator fun iterator(): Iterator<Animal> { // 1 - Operador de funçao iterator. Permite que a classe seja iterável (percorrida por uma estrutura de repetição)
+        return animals.iterator()               // 2 - Permite iterar a lista de animais ( do construtor da classe Zoo )
     }
 }
 
 fun main() {
 
-    val zoo = Zoo(listOf(Animal("zebra"), Animal("lion")))
+    val animals = listOf(Animal("zebra"), Animal("lion")) // lista de animais
+    val zoo = Zoo(animals)
 
-    for (animal in zoo) {                                   // 3
+    for (animal in zoo) { // 3 - For percorre zoo (o que retorna do iterator)
         println("Watch out, it's a ${animal.name}")
     }
 
